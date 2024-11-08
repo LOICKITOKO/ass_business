c051fa6903874c9bddbaeeb20b19d6404b4e1083
LOICKITOKO <loicassaelkm@gmail.com>
2024-11-08 15:05:42 +0100

    all task

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import AlbumViewSet, ArtistViewSet, SongViewSet, CommentViewSet
 
+# Initialisation du routeur DRF
 router = DefaultRouter()
 router.register(r'albums', AlbumViewSet)
 router.register(r'artists', ArtistViewSet)
 router.register(r'songs', SongViewSet)
 router.register(r'comments', CommentViewSet)
 
+# Définition des URL
 urlpatterns = [
-    path('api/', include(router.urls)),  # Inclure toutes les routes définies par le router
+    path('api/', include(router.urls)),  # Inclure toutes les routes définies par le routeur
 ]
